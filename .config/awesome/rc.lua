337a99800a920760c0c0679edeb68231f58c3f83
Daniel Fanjul <daniel.fanjul.alcuten@gmail.com>
2014-09-27 13:23:37 +0100

    update .xprofile

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -1,13 +1,3 @@
--- Override awesome.quit when we're using GNOME
-_awesome_quit = awesome.quit
-awesome.quit = function()
-  if os.getenv("DESKTOP_SESSION") == "awesome-gnome" then
-    os.execute("/usr/bin/gnome-session-quit")
-  else
-    _awesome_quit()
-  end
-end
-
 -- Standard awesome library
 require("awful")
 require("awful.autofocus")
